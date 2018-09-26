d7d220be27540524f689506daca2eba3cb11782b
Administrator <administrator@Rafaels-MBP.cmts.eph.ptd.net>
2018-09-25 23:35:29 -0400

    setting up code needed for the interpreter, to be able to visualize data

---
+++ jupyter_inputs.py
@@ -0,0 +1,8 @@
+'''
+	- This code is to be run inside of the python interpreter (Jupyter Notebook)
+	- The first line imports both matplotlib and NumPy modules
+	- The second and third line are for the data visualization libraries
+'''
+%pylab inline 
+import pandas 
+import seaborn
